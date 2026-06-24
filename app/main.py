c1e6cb1e22710ac71363e0a1317ad1380f129525
jofezs <josefbinod@gmail.com>
2026-06-24 16:22:30 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="My Azure API",
+    description="A FastAPI application deployed on Azure App Service",
+    version="1.0.0",
+)
+
+
+@app.get("/")
+def read_root():
+    return {
+        "status": "online",
+        "message": "FastAPI is running on Azure App Service",
+    }
+
+
+@app.get("/helloworld")
+def hello_world():
+    return {"message": "Hello World"}
